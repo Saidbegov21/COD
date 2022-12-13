e68beac62fd3e0453e093ae3e42f7ca92a0c2d07
SaidbegovRamazan <ramazan113@bk.ru>
2022-12-13 16:53:54 +0300

    my message

---
+++ IF/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+public class NewBaseType
+{
+    public static void Main()
+    {
+        int t (int m, int n)
+        {
+            if (m == 0)
+            {
+                return n + 1;
+            }
+            if (m > 0 && n == 0)
+            {
+                return t (m - 1, 1);
+            }
+            return t (m - 1, t(m, n - 1));
+        }
+        Console.WriteLine(t (3, 2));
+    }
+}
+
+public class Program : NewBaseType
+{
+}
---
+++ IP/Program.cs
@@ -0,0 +1,23 @@
+﻿internal class NewBaseType
+{
+    private static void Main(string[] args)
+    {
+        int sum (int m, int n)
+        {
+            int start = m;
+            int end = n;
+            if (m > n)
+            {
+                start = n;
+                end = m;
+            }
+            return (end + start) * (end - start + 1) / 2; //Формула
+        }
+
+        Console.WriteLine(sum(72, 16));
+    }
+}
+
+internal class Program : NewBaseType
+{
+}
